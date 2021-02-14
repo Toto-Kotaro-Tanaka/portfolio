0e82631afe4b74b6be65392125a982c0ca5fb9dc
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-02-14 19:54:08 +0000

    Update index.html and style style.css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,24 +26,6 @@ const showHeadingDesc = function (target, message, index, interval) {
 };
 
 showHeadingDesc("#heading-desc", "a full stack developer...", 0, 200);
-
-// const showMainText = function (target, message, index, interval) {
-//     setTimeout(function () {
-//         if (index < message.length) {
-//             document.querySelector(target).append(message[index++]);
-//             setTimeout(function () {
-//                 showHeadingName(target, message, index, interval);
-//             }, interval);
-//         }
-//     }, 9500);
-// };
-
-// showMainText(
-//     "#main-text",
-//     "here is the link to my projects. please take a look at them",
-//     0,
-//     50
-// );
 // ---------- /End of Credit ----------
 
 // Footer Year Auto Update
