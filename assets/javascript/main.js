a475fa579c4d71b6f128549ed82b10bbd55f0b90
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-02-16 20:37:12 +0000

    Test bubble speech

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -28,9 +28,6 @@ const showHeadingDesc = function (target, message, index, interval) {
 showHeadingDesc("#heading-desc", "a full stack developer...", 0, 200);
 // ---------- /End of Credit ----------
 
-// Footer Year Auto Update
-$("#copyright").text(new Date().getFullYear());
-
 // Credit: Stack Overflow @ https://stackoverflow.com/questions/10385950/how-to-get-a-div-to-randomly-move-around-a-page-using-jquery-or-css
 // UFO
 
@@ -42,7 +39,6 @@ function makeNewPosition() {
     // Get viewport dimensions (remove the dimension of the div)
     let h = $(window).height() - 50;
     let w = $(window).width() - 50;
-
     let nh = Math.floor(Math.random() * h);
     let nw = Math.floor(Math.random() * w);
 
@@ -62,12 +58,91 @@ function animateDiv() {
 function calcSpeed(prev, next) {
     let x = Math.abs(prev[1] - next[1]);
     let y = Math.abs(prev[0] - next[0]);
-
     let greatest = x > y ? x : y;
-
     let speedModifier = 0.08;
-
     let speed = Math.ceil(greatest / speedModifier);
 
     return speed;
 }
+// ---------- /End of Credit ----------
+
+// Footer Year Auto Update
+$("#copyright").text(new Date().getFullYear());
+
+// ------------------
+// ------------------
+
+// IIFE to ensure safe use of $
+(function ($) {
+    // Create plugin
+    $.fn.tooltips = function (el) {
+        var $tooltip,
+            $body = $("body"),
+            $el;
+
+        // Ensure chaining works
+        return this.each(function (i, el) {
+            $el = $(el).attr("data-tooltip", i);
+
+            // Make DIV and append to page
+            var $tooltip = $(
+                '<div class="tooltip" data-tooltip="' +
+                    i +
+                    '">' +
+                    $el.attr("title") +
+                    '<div class="arrow"></div></div>'
+            ).appendTo("body");
+
+            // Position right away, so first appearance is smooth
+            var linkPosition = $el.position();
+
+            $tooltip.css({
+                top: linkPosition.top - $tooltip.outerHeight() - 13,
+                left: linkPosition.left - $tooltip.width() / 2,
+            });
+
+            $el
+                // Get rid of yellow box popup
+                .removeAttr("title")
+
+                // Mouseenter
+                .hover(
+                    function () {
+                        $el = $(this);
+
+                        $tooltip = $(
+                            "div[data-tooltip=" + $el.data("tooltip") + "]"
+                        );
+
+                        // Reposition tooltip, in case of page movement e.g. screen resize
+                        var linkPosition = $el.position();
+
+                        $tooltip.css({
+                            top: linkPosition.top - $tooltip.outerHeight() - 13,
+                            left: linkPosition.left - $tooltip.width() / 2,
+                        });
+
+                        // Adding class handles animation through CSS
+                        $tooltip.addClass("active");
+
+                        // Mouseleave
+                    },
+                    function () {
+                        $el = $(this);
+
+                        // Temporary class for same-direction fadeout
+                        $tooltip = $(
+                            "div[data-tooltip=" + $el.data("tooltip") + "]"
+                        ).addClass("out");
+
+                        // Remove all classes
+                        setTimeout(function () {
+                            $tooltip.removeClass("active").removeClass("out");
+                        }, 300);
+                    }
+                );
+        });
+    };
+})(jQuery);
+
+$("a[title]").tooltips();
